6ac22f40064b75670dccb6052150b2aaf15c500d
daenv <daenvle69@gmail.com>
2023-02-27 10:44:15 +0700

    remember fix bugs order status of checkout model / 27/2/2023

--- src/v1/models/orders/order.model.ts
+++ src/v1/models/orders/order.model.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { User } from "../users/user.model";
-import { Product, ProductSchema } from '../products/product.model';
+import { Product, ProductSchema } from "../products/product.model";
 import { Payment } from "../payments/payment.model";
 export interface Order extends mongoose.Document {
   user_id: User["_id"];
@@ -11,15 +11,13 @@ export interface Order extends mongoose.Document {
   order_date: Date;
   status: string[];
 }
-export const OrderSchema: mongoose.Schema<Order> = new mongoose.Schema(
-    {
-        user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-        product_list: [{ type: [ProductSchema], ref: "Product" }],
-        total_amount: { type: Number, required: true },
-        payment_method: { type: String, required: true, enum: ["credit_card", "cash_on_delivery", "paypal"] },
-        delivery_address: { type: String, required: true },
-        order_date: { type: Date, default: Date.now },
-        status: { type: [String], required: true, enum: ["pending", "success", "failed"] },
-    }
-)
+export const OrderSchema: mongoose.Schema<Order> = new mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  product_list: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true }],
+  total_amount: { type: Number, required: true },
+  payment_method: { type: String, required: true, enum: ["credit_card", "cash_on_delivery", "paypal"] },
+  delivery_address: { type: String, required: true },
+  order_date: { type: Date, default: Date.now },
+  status: { type: [String], required: true, enum: ["pending", "success", "failed"] },
+});
 export const OrderModel = mongoose.model<Order>("Order", OrderSchema);
