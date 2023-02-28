1fd6bff1d6bf98d7d76f17118a66ea16617bd122
daenv <daenvle69@gmail.com>
2023-02-28 10:29:28 +0700

    28/2023

--- src/v1/models/users/user.model.ts
+++ src/v1/models/users/user.model.ts
@@ -73,6 +73,19 @@ export const UserSchema: mongoose.Schema<User> = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      tvrim: true,
+      validate: {
+        validator: (value: string) => {
+          // Kiểm tra giá trị của trường username với một biểu thức chính quy
+          return /^[a-zA-Z0-9]+$/.test(value);
+        },
+        message: "Username không hợp lệ",
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
       minlength: 6,
       lowercase: true,
       trim: true,
@@ -83,13 +96,22 @@ export const UserSchema: mongoose.Schema<User> = new mongoose.Schema(
         message: "Email not valid",
       },
     },
-    email: { type: String, required: true },
-    password: { type: String, required: true, minlength: 6 },
+    password: { type: String, required: true, minlength: 6, trim: true },
     role: { type: Boolean, required: true, default: false },
-    fist_name: { type: String, required: true },
-    last_name: { type: String, required: true },
+    fist_name: { type: String, required: true, trim: true },
+    last_name: { type: String, required: true, trim: true },
     image: { type: String, required: true },
-    phone: { type: String, required: true },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: (value: string) => {
+          return /^(\+84|0)[1-9][0-9]{8}$/.test(value);
+        },
+        message: "Phone not valid",
+      },
+    },
     address: { type: String, required: true },
     sex: { type: String, required: true },
     birthday: { type: Date, required: true },
