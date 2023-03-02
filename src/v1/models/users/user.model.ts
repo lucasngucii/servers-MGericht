77d16dfe3b5fec4574d036b695eb44915e256064
daenv <daenvle69@gmail.com>
2023-03-02 11:02:39 +0700

    2/3/2023

--- src/v1/models/users/user.model.ts
+++ src/v1/models/users/user.model.ts
@@ -4,12 +4,24 @@ export interface user extends mongoose.Document {
   email: string;
   username: string;
   password: string;
+  first_name: string;
+  last_name: string;
+  phone: number;
+  address: string[];
+  role: boolean;
+  createdAt: Date;
 }
 export const userSchema: mongoose.Schema<user> = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     username: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true, trim: true },
+    first_name: { type: String, required: true, trim: true },
+    last_name: { type: String, required: true, trim: true },
+    phone: { type: Number, required: true, trim: true },
+    address: { type: [String], required: true, trim: true },
+    role: { type: Boolean, required: true, trim: true },
+
   },
   {
     timestamps: true,
