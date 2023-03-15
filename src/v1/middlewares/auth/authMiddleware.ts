efa37a7bc11b7c8716a11b7b1a375c4404854d0b
daenv <daenvle69@gmail.com>
2023-03-15 21:06:30 +0700

    15/3/2023g

--- src/v1/middlewares/auth/authMiddleware.ts
+++ src/v1/middlewares/auth/authMiddleware.ts
@@ -1,18 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 import { getErrorMessage } from "../../utils/error/errorMessage";
 import { HTTP_BAD_REQUEST, HTTP_INTERNAL_SERVER_ERROR } from "../../constants/http-status/http_status";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import * as userService from "../../services/users/User.service";
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   if (req?.headers?.authorization?.startsWith("Bearer")) {
-      const token = req.headers.authorization.split("Bearer ")[1];
-      const secrect = process.env.JWT_SECRET || "secret-key";
+    const token = req.headers.authorization.split("Bearer ")[1];
+    const secrect = process.env.JWT_SECRET || "secret-key";
     try {
       if (token) {
         const decoded = jwt.verify(token, secrect);
-        console.log(decoded);
-        next()
+        //@ts-ignore
+        const user = await userService.getUserById(decoded?.id);
+        //@ts-ignore
+        req.user = user;
+        next();
       }
     } catch (error) {
       res.status(HTTP_BAD_REQUEST).json({ message: getErrorMessage(error) });
