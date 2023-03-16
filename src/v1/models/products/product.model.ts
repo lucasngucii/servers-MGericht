90d0bdad70f027cc13be414f82463ce0bc47de5f
daenv <daenvle69@gmail.com>
2023-03-16 07:29:44 +0700

    16/3/2023

--- src/v1/models/products/product.model.ts
+++ src/v1/models/products/product.model.ts
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 import { tagSchema, tag } from "../tags/tag.model";
+import { user } from "../users/user.model";
+export interface rating extends mongoose.Document {
+  userId: user["_id"];
+  rate: number;
+  comment: string;
+}
+export const ratingSchema: mongoose.Schema<rating> = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  rate: { type: Number, default: 0 },
+  comment: { type: String, default: "" },
+});
 export interface Product extends mongoose.Document {
   name: string;
   price: number;
@@ -7,6 +18,7 @@ export interface Product extends mongoose.Document {
   image: string;
   category: string;
   tags: tag[];
+  ratings: rating[];
 }
 export const ProductSchema: mongoose.Schema<Product> = new mongoose.Schema(
   {
@@ -16,6 +28,7 @@ export const ProductSchema: mongoose.Schema<Product> = new mongoose.Schema(
     image: { type: String, required: true },
     category: { type: String, required: true },
     tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "tag", required: true }],
+    ratings: [{ type: ratingSchema, default: { rate: 0, comment: "" } }],
   },
   {
     timestamps: true,
