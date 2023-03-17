46431f1ab5ff1fdca7074f39cf4d98bf4d7d86cd
daenv <daenvle69@gmail.com>
2023-03-17 17:40:20 +0700

    17/3/2023

--- src/v1/controllers/carts/cart.controller.ts
+++ src/v1/controllers/carts/cart.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { HTTP_INTERNAL_SERVER_ERROR } from "../../constants/http-status/http_status";
+import { HTTP_INTERNAL_SERVER_ERROR, HTTP_SUCCESS } from "../../constants/http-status/http_status";
 import { getErrorMessage } from "../../utils/error/errorMessage";
 import * as cartService from "../../services/carts/cart.service";
 export const userCart = async (req: Request, res: Response, next: NextFunction) => {
@@ -7,9 +7,9 @@ export const userCart = async (req: Request, res: Response, next: NextFunction) 
   const userId = req.user._id;
   const { productId } = req.body;
   try {
-      const cart = await cartService.addProductToCart(userId, productId);
-
+    const cart = await cartService.addProductToCart(userId, productId);
+    res.status(HTTP_SUCCESS).json({ cart });
   } catch (error) {
-    res.status(HTTP_INTERNAL_SERVER_ERROR).json({message: getErrorMessage(error)});
+    res.status(HTTP_INTERNAL_SERVER_ERROR).json({ message: getErrorMessage(error) });
   }
 };
--- src/v1/services/carts/cart.service.ts
+++ src/v1/services/carts/cart.service.ts
@@ -1,18 +1,36 @@
-
-import { DocumentDefinition } from "mongoose"
-import { Cart, CartModel } from '../../models/carts/cart.model';
-import { user, userModel } from '../../models/users/user.model';
-import { Product, productModel } from "../../models/products/product.model"
+import { DocumentDefinition } from "mongoose";
+import { Cart, CartModel, cartItem, CartSchema } from "../../models/carts/cart.model";
+import { user, userModel } from "../../models/users/user.model";
+import { Product, productModel } from "../../models/products/product.model";
 
 export const addProductToCart = async (userId: string, productId: string) => {
-    try {
-        const user = await userModel.findById(userId).select("-password").exec();
-        if (!user) throw new Error("User not found");
-        const product = await productModel.findById(productId).exec();
-        if (!product) throw new Error("Product not found");
-        const itemAdded = user.cart.find((item)=> console.log(item));
-        
-    } catch (error) {
-        throw error;
+  try {
+    const user = await userModel.findById(userId).select("-password").exec();
+    if (!user) throw new Error("User not found");
+    const product = await productModel.findById(productId).exec();
+    if (!product) throw new Error("Product not found");
+    const itemAdded = await CartModel.findOne({ user_id: user._id, "items.product_id": product._id });
+    
+    if (itemAdded) { 
+      
+
     }
-} 
+    
+
+    const newCart = {
+      user_id: user._id,
+      items: [
+        {
+          product_id: product._id,
+          quantity: 1,
+          price: product.price,
+        },
+      ],
+    };
+    const cart = await CartModel.create(newCart);
+    await cart.save();
+    return cart;
+  } catch (error) {
+    throw error;
+  }
+};
