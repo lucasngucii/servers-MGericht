e66d4ac9e6376afe927a7baef4a0e57c1890f4f3
daenv <daenvle69@gmail.com>
2023-03-17 19:59:26 +0700

    17/3/2023

--- src/v1/services/carts/cart.service.ts
+++ src/v1/services/carts/cart.service.ts
@@ -9,14 +9,15 @@ export const addProductToCart = async (userId: string, productId: string) => {
     if (!user) throw new Error("User not found");
     const product = await productModel.findById(productId).exec();
     if (!product) throw new Error("Product not found");
-    
-    
-    const newCart = {
-      user_id: user._id,
-      items: [
-        {
-          product_id: product._id,
-          quantity: 1,
+    //check product in cart
+
+
+      const newCart = {
+        user_id: user._id,
+        items: [
+          {
+            product_id: product._id,
+            quantity: 1,
           price: product.price,
         },
       ],
