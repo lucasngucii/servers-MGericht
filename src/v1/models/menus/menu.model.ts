0e17c450214a4effa936b38041107ae3462960a7
daenv <daenvle69@gmail.com>
2023-03-17 20:50:21 +0700

    17/3/2023

---
+++ src/v1/models/menus/menu.model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { Product } from "../products/product.model";
+
+export interface menu extends mongoose.Document {
+  name: string;
+  price: number;
+  description: string;
+  item: Product["_id"];
+  image: string;
+}
+export const MenuSchema: mongoose.Schema<menu> = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number },
+  description: { type: String, required: true },
+  item: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    image: { type: String, },
+});
+
+export const MenuModel: mongoose.Model<menu> = mongoose.model("Menu", MenuSchema);
