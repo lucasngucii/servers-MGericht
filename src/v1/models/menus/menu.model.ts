3b889eb19a6c77a8d24eb5d8d4ac756e26a916a5
daenv <daenvle69@gmail.com>
2023-03-18 14:50:03 +0700

    18/3/2023

--- src/v1/models/menus/menu.model.ts
+++ src/v1/models/menus/menu.model.ts
@@ -5,15 +5,15 @@ export interface menu extends mongoose.Document {
   name: string;
   price: number;
   description: string;
-  item: Product["_id"];
+  item: Product[];
   image: string;
 }
 export const MenuSchema: mongoose.Schema<menu> = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number },
   description: { type: String, required: true },
-  item: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-    image: { type: String, },
+  item: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true }],
+  image: { type: String },
 });
 
 export const MenuModel: mongoose.Model<menu> = mongoose.model("Menu", MenuSchema);
