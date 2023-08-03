9ff4c2e64cd360061a24a94c991fda58e6fee501
daenv <daenvle69@gmail.com>
2023-08-03 10:20:33 +0700

    add admin router

--- src/v1/routes/coupons/coupon.router.ts
+++ src/v1/routes/coupons/coupon.router.ts
@@ -1,17 +1,18 @@
-import { Router } from "express";
-import { authMiddleware } from "../../middlewares/auth/authMiddleware";
-import * as couponController from "../../controllers/coupons/coupon.controller";
+import { Router } from 'express';
+import { authMiddleware } from '../../middlewares/auth/authMiddleware';
+import * as couponController from '../../controllers/coupons/coupon.controller';
+import { isAdmin } from '../../middlewares/auth/isAdmin';
 const router = Router();
 //GET /coupons: Lấy danh sách các mã giảm giá hiện có.
-router.get("/", authMiddleware, couponController.getCoupons);
+router.get('/', authMiddleware, couponController.getCoupons);
 //GET /coupons/{id}: Lấy thông tin chi tiết về một mã giảm giá cụ thể dựa trên ID.
-router.get("/:id", authMiddleware, couponController.getCouponById);
+router.get('/:id', authMiddleware, couponController.getCouponById);
 //GET /coupons/validate/{couponId}: Kiểm tra mã giảm giá có hợp lệ hay không.
-router.get("/validate/:id", authMiddleware, couponController.validateCoupon);
+router.get('/validate/:id', authMiddleware, couponController.validateCoupon);
 //POST /coupons: Tạo một mã giảm giá mới.
-router.post("/", authMiddleware, couponController.createCoupon);
+router.post('/', authMiddleware, isAdmin, couponController.createCoupon);
 //PUT /coupons/{id}: Cập nhật thông tin của một mã giảm giá cụ thể dựa trên ID.
-router.put("/:couponId", authMiddleware, couponController.updateCoupon);
+router.put('/:couponId', authMiddleware, isAdmin, couponController.updateCoupon);
 //DELETE /coupons/{id}: Xóa một mã giảm giá cụ thể dựa trên ID.
-router.delete("/:couponId", authMiddleware, couponController.deleteCoupon);
-module.exports = router; 
+router.delete('/:couponId', authMiddleware, isAdmin, couponController.deleteCoupon);
+module.exports = router;
