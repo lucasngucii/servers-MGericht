661aa3f5a1b46943a2f1a58bcb6fa8dd9b74ecae
daenv <daenvle69@gmail.com>
2024-03-05 23:32:04 +0700

    Refactor database connection code

--- src/v1/database/connectdb.ts
+++ src/v1/database/connectdb.ts
@@ -1,11 +1,29 @@
-const mongoose = require("mongoose");
-const uri = process.env.MONGO_URI;
-const dbName = process.env.DB_NAME;
-// fix strictQuery
-mongoose.set("strictQuery", false);
-// connect mongoose
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, dbName })
-  .then(() => console.log("Connected to Mongoose seccesfully ......"))
-  .catch((err: any) => console.error(`Error: ${err}`));
-module.exports = mongoose;
+import * as mongoose from 'mongoose';
+
+const connectString = `mongodb://localhost:27017/sleeprDEV`;
+
+//using design pattern singletons
+class Database {
+   private static instance: Database | null = null;
+   constructor() {
+      this._connect();
+   }
+   private _connect(type = 'mongodb'): Promise<void> {
+      mongoose.set('debug', true);
+      mongoose.set('debug', { color: true });
+
+      return mongoose
+         .connect(connectString, { maxPoolSize: 100 })
+         .then(() => console.log('Connected to MongoDB'))
+         .catch(() => console.log(' connection failed'));
+   }
+   public static getInstance(): Database {
+      if (!Database.instance) {
+         Database.instance = new Database();
+      }
+      return Database.instance;
+   }
+}
+const instanceMongoDB = Database.getInstance();
+
+export default instanceMongoDB;
