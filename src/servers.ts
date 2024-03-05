fc0bfff38420019b3884ee25cd89d463245ce0e3
daenv <daenvle69@gmail.com>
2024-03-05 23:31:57 +0700

    Update server port configuration

--- src/servers.ts
+++ src/servers.ts
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const server = require('./v1/app.ts');
-const port =  process.env.PORT;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log(`Server start on ${port}`);
+   console.log(`Server start on ${port}`);
 });
-/* process.on('SIGINT', () => {
-  server.close(() => console.log(`exits server express`));
+process.on('SIGINT', () => {
+   server.close(() => {
+      console.log('Server closed');
+      process.exit(2);
+   });
 });
- */
